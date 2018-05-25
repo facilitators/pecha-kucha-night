c08e28732f2668d6a013bb4f73a4adbc096eb1ef
mauodias <mau.dias@gmail.com>
2018-05-25 18:21:23 -0300

    [ @mauodias / @hscorrea ] Add new test to check if CircleCI is working

--- test/spike.js
+++ test/spike.js
@@ -4,4 +4,11 @@ describe('Quando rodar a aplicação', function () {
   it('A aplicação deve rodar', function () {
     expect(true).to.eq(true)
   })
-})
+});
+
+
+describe('Quando esse outro teste rodar', function() {
+  it('A aplicação já vai estar rodando', function() {
+    expect(false).to.eq(false)
+  })
+});
